5eed8091a1b154d87dce42ac1ffd50daf56456c1
HNoseery <hassan.alnoseery@stud.tu-darmstadt.de>
2025-02-11 22:11:05 +0100

    Assignment 1a. Created variables for the HDF5-file file path, brewing-ID and the Tank-ID.

---
+++ main.py
@@ -0,0 +1,23 @@
+import datetime
+import os
+
+import numpy as np
+
+import project.functions as fn
+from project.functions import read_metadata
+
+
+def main():
+
+    file_path = "/Users/HNoseery/Desktop/pa-ws2425/project/data/data_GdD_Datensatz_WS2425.h5"
+    brewing = "brewing_0001"
+    tank_id = "B002"
+
+
+
+
+
+
+if __name__ == "__main__":
+    main()
+

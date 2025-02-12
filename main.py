e135e7c5bc5ef1df75876445cb5406db4994b136
HNoseery <hassan.alnoseery@stud.tu-darmstadt.de>
2025-02-12 12:27:59 +0100

    Assignment 1D. In the main.py Script, recalled the read_metadata funktion to extract the Meta data T_env, specific_heat_capacity and the density_beer from under the Brewing group

--- main.py
+++ main.py
@@ -13,8 +13,16 @@ def main():
     brewing = "brewing_0001"
     tank_id = "B002"
 
-measured_quantities = ("level", "temperature", "timestamp")
+    measured_quantities = ("level", "temperature", "timestamp")
 
+    brewing_group_path = "brewing_0001"
+
+
+    T_env = read_metadata(file_path, brewing_group_path, "T_env")
+
+    specific_heat_capacity_beer = read_metadata(file_path,brewing_group_path,"specific_heat_capacity_beer")
+
+    density_beer = read_metadata(file_path,brewing_group_path,"density_beer")
 
 
 
